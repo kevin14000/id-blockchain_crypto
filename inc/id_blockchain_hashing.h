934b383beca0fd8060b923b29c9e319914ae405e
Morgan Barbier <morgan.barbier@ensicaen.fr>
2018-06-29 11:30:32 +0200

    Ajout de la doc doxygen pour inc/id_blockchain_hashing.h et la presentation

--- inc/id_blockchain_hashing.h
+++ inc/id_blockchain_hashing.h
@@ -1,28 +1,53 @@
+/**
+ * \file id_blockchain_hashing.h
+ *
+ * \brief Header file for containing the hash functions,
+ *        that is SHA256 and SHA512.
+ *
+ * \auhtors Kevin Atighehchi <kevin.atighehchi@unicaen.fr>
+ *          Morgan Barbier <morgan.barbier@ensicaen.fr>
+ *
+ * \version 1.0
+ *
+ * \date 2018-06-29
+ */
+
 #ifndef ID_BLOCKCHAIN_HASHING_H
 #define ID_BLOCKCHAIN_HASHING_H
 
 #include <openssl/sha.h>
 
 
-/* Fonction de hachage avec une sortie de 256 bits, pour un niveau de sécurité
-   de 128 bits (résistance aux collisions). Elle prend trois paramètres dont 
-   une sortie.
-	 Entrées : Une chaîne d'octets (entièrement en mémoire)
-	 					 La longueur de cette chaîne, en nombre d'octets
-   Retour :  Le condensé en paramètre, une chaîne de 32 octets
+/**
+ * \fn void id_blockchain_sha256(const unsigned char *byte_string, size_t len, unsigned char digest[SHA256_DIGEST_LENGTH]);
+ *
+ * \brief Hash function of 256 output bits, for a security level of 128 bits
+ *        (collision resilient). It takes three input parameters of which
+ *        to write the result
+ *
+ * \param[in] byte_string is the byte string to hash
+ * \param[in] len is the length of byte_string
+ * \param[in] digest[] is the output of 32 bytes
+ *
 */
-void id_blockchain_sha256(const unsigned char *byte_string, 
-												  size_t len,
-												  unsigned char digest[SHA256_DIGEST_LENGTH]);
-												  
-/* Fonction de hachage avec une sortie de 512 bits, pour un niveau de sécurité
-   de 256 bits. Elle prend trois paramètres dont une sortie.
-	 Entrées : Une chaîne d'octets (entièrement en mémoire)
-	 					 La longueur de cette chaîne, en nombre d'octets
-   Retour :  Le condensé en paramètre, une chaîne de 64 octets
+void id_blockchain_sha256(const unsigned char *byte_string,
+			  size_t len,
+			  unsigned char digest[SHA256_DIGEST_LENGTH]);
+
+/**
+ * \fn void id_blockchain_sha512(const unsigned char *byte_string, size_t len, unsigned char digest[SHA512_DIGEST_LENGTH]);
+ *
+ * \brief Hash function of 512 output bits, for a security level of 256 bits
+ *        (collision resilient). It takes three input parameters of which
+ *        to write the result
+ *
+ * \param[in] byte_string is the byte string to hash
+ * \param[in] len is the length of byte_string
+ * \param[in] digest[] is the output of 64 bytes
+ *
 */
 void id_blockchain_sha512(const unsigned char *byte_string, 
-													size_t len,
-													unsigned char digest[SHA512_DIGEST_LENGTH]);
-																		
-#endif
+			  size_t len,
+			  unsigned char digest[SHA512_DIGEST_LENGTH]);
+
+#endif /* ID_BLOCKCHAIN_HASHING_H */
